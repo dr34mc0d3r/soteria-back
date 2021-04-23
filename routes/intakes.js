9420f5667eddce13ab00197844b906db145e07a0
Chris <root.operator@gmail.com>
2021-04-23 15:35:20 -0400

    modify intake routes

--- routes/intakes.js
+++ routes/intakes.js
@@ -5,8 +5,8 @@ const checkAuthMiddleware = require('../middleware/check-auth');
 const router = express.Router();
 
 router.post("/", checkAuthMiddleware.checkAuth, intakeontroller.save);
-router.get("/myintakes/:id", intakeontroller.index);
-router.get("/:id", intakeontroller.show);
+router.get("/myintakes/:id", checkAuthMiddleware.checkAuth, intakeontroller.index);
+router.get("/:id", checkAuthMiddleware.checkAuth, intakeontroller.show);
 router.patch("/:id", checkAuthMiddleware.checkAuth, intakeontroller.update);
 router.delete("/:id", checkAuthMiddleware.checkAuth, intakeontroller.destroy);
 
